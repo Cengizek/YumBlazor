c5b7d09592464d46c07bb4a1dd244256a031c59c
Cengizek <ekmekcicengiz64.com>
2025-05-11 20:02:52 +0300

    Last Updated

---
+++ Utility/SD.cs
@@ -0,0 +1,38 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Utility
+{
+    public static class SD
+    {
+        public static string Role_Customer = "Customer";
+        public static string Role_Admin = "Admin";
+
+        public static string StatusPending = "Pending";
+        public static string StatusReadyToPickUp = "ReadyToPickUp";
+        public static string StatusCompleted = "Completed";
+        public static string StatusCancelled = "Cancelled";
+
+        public static List<OrderDetail> ConvertToShoppingCart(List<ShoppingCart> shoppingCarts) {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            foreach (var cart in shoppingCarts)
+            {
+                OrderDetail orderDetail = new OrderDetail
+                {
+                    ProductId = cart.ProductId,
+                    Count = cart.Count,
+                    Price = Convert.ToDouble(cart.Product.Price),
+                    ProductName = cart.Product.Name,
+                };
+                orderDetails.Add(orderDetail);
+            }
+            return orderDetails;
+
+        }
+            
+
+
+         
+
+
+    }
+}
